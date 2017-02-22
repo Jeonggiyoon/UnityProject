9b3fe1ecf040965dd6e317c82a760fdf8fae0ddb
jeong <rldbs0802@naver.com>
2017-02-22 11:16:34 +0900

    home Clone

---
+++ src/Assets/Scripts/Fly_Drone/SpawnerManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerManager : MonoBehaviour {
+
+    Vector3[] positions = new Vector3[5];
+    public GameObject obj;
+    public bool isSpawn = true;
+    public float spawnDelay = 1.5f;
+    float spawnTimer = 0f; 
+
+	// Use this for initialization
+	void Start () {
+        CreatePositions();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        SpawnDrone();
+	}
+
+    void SpawnDrone()
+    {
+        if (isSpawn == true)
+        {
+            if (spawnTimer > spawnDelay)
+            {
+                int rand = Random.Range(0, positions.Length);
+                Instantiate(obj, positions[rand], Quaternion.identity);
+                spawnTimer = 0f;
+            }
+            spawnTimer += Time.deltaTime;
+        }
+    }
+
+    void CreatePositions()
+    {
+        float viewPosY = 1.2f;
+        float viewPosX = 0f;
+        float gapX = 1f / 6f;
+
+        for (int i = 0; i < positions.Length; i++)
+        {
+            viewPosX = gapX + gapX * i;
+            Vector3 viewPos = new Vector3(viewPosX, viewPosY, 0);
+            Vector3 worldPos = Camera.main.ViewportToWorldPoint(viewPos);
+            worldPos.z = 0f;
+            positions[i] = worldPos;
+            print(worldPos);
+        }
+    }
+
+}
